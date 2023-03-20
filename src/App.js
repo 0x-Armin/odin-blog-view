7c787eb36f282a3b3d9319a05f550006a82a4758
pohjie <0xArlet@gmail.com>
2023-03-20 19:46:45 +0800

    Allow user to view specific post and comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+  useParams,
+} from "react-router-dom";
+
 import api from "./api";
 
 function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Blog</h1>
+        <Routes>
+          <Route exact path="/posts" element={<Home />} />
+          <Route path="/posts/:id" element={<Post />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
   const [blogPosts, setBlogPosts] = useState([]);
 
   useEffect(() => {
     api
       .get("/posts")
       .then((response) => {
-        console.log(response.data);
         setBlogPosts(response.data);
       })
       .catch((error) => console.error(error));
@@ -16,13 +38,53 @@ function App() {
 
   return (
     <div>
-      <h1>Blog</h1>
       <ul>
-      {blogPosts.map(item => <li key={item.id}>
-        <h3>{item.title}</h3>
-        <p>{item.date_formatted}</p>
-        <p>{item.content}</p>
-        </li>)}
+        {blogPosts.map((post) => (
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+            <p>{post.date_formatted}</p>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Post() {
+  const { id } = useParams();
+  const [postPackage, setPostPackage] = useState(null);
+
+  useEffect(() => {
+    api
+      .get(`/posts/${id}`)
+      .then((response) => {
+        console.log(response.data);
+        setPostPackage(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
+  if (!postPackage) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{postPackage.post.title}</h2>
+      <p>{postPackage.post.date_formatted}</p>
+      <p>{postPackage.post.content}</p>
+      <h3>Comments</h3>
+      <ul>
+      {postPackage.comments.map((comment) => (
+          <li key={comment.id}>
+            <h4>{comment.username}</h4>
+            <p>{comment.date_formatted}</p>
+            <p>{comment.content}</p>
+          </li>
+        ))} 
       </ul>
     </div>
   );
